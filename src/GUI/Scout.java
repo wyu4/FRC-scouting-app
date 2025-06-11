96bb61e985647299917fd84f6b301a22b4ef9966
Erin <enguy4@ocdsb.ca>
2025-06-11 14:37:32 -0400

    Add files via upload

---
+++ src/GUI/Scout.java
@@ -0,0 +1,87 @@
+package GUI;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+public class Scout extends JFrame implements ActionListener {
+    int coralCounter = 0;
+    boolean hello = true;
+    private Image backgroundImage;
+    private JPanel backgroundPanel;
+    static JFrame frame;
+
+    JButton start, data, exit, home;
+
+
+
+
+public Scout(){
+
+    setBounds(100, 200, 1500, 900);
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
+    setLayout(null);
+
+    backgroundPanel = new BackgroundPanel();
+    backgroundPanel.setBounds(0, 0, 1500, 900);
+    backgroundPanel.setLayout(null);
+    setContentPane(backgroundPanel);
+
+        JButton start = new JButton();
+    start.setBounds(240,430,375,225);
+    start.setVisible(true);
+    start.setOpaque(true);
+    start.setContentAreaFilled(true);
+    start.setBorderPainted(true);
+    start.addActionListener(this);
+
+
+
+    backgroundPanel.add(start);
+    setVisible(true);
+
+
+
+
+
+
+}
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == start) {
+            new ScoutScreen();
+            dispose();
+        }
+
+     
+        if (e.getSource() == data) {
+            data.setText("Yipee");
+
+        }
+
+    } 
+
+    public static void main(String[] args){
+        new Scout();
+
+    }
+
+
+
+}
+
+ class BackgroundPanel extends JPanel {
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Image background = new ImageIcon("C:/Users/enchi/.vscode/Scouting app/src/GUI/title.png").getImage();
+        g.drawImage(background, 0, 0, getWidth(), getHeight(), this);
+    }
+}
---
+++ src/GUI/main.java
@@ -0,0 +1,11 @@
+package GUI;
+import java.util.Arrays;
+import java.io.*;
+import java.util.Scanner;
+
+
+public class main {
+    public static void main(String[] args){
+        new ScoutScreen();
+    }
+}
